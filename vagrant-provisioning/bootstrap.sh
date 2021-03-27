5b7e15ddf02b1c4c01c5555e50445cb6d26ee7e3
santhoshb1987 <38525179+santhoshb1987@users.noreply.github.com>
2021-03-27 16:18:45 +0530

    Update bootstrap.sh

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -55,7 +55,6 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster.example.com     kmaster
-172.16.16.101   kworker1.example.com    kworker1
-172.16.16.102   kworker2.example.com    kworker2
+192.168.56.2   kmaster.example.com     kmaster
+192.168.56.3   kworker1.example.com    kworker1
 EOF
